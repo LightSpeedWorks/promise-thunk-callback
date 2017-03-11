5691b41789488cca130bf7e8566f09952d7280c3
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-03-11 21:06:02 +0900

    remove

--- examples/another6-thunk-promise-callback.js
+++
@@ -1,9 +0,0 @@
-process.title = __filename.substr(__dirname.length + 1);
-(function () {
-	'use strict';
-
-	const Thunk = require('./thunk2');
-	const benchAll = require('./bench-all');
-	benchAll(Thunk);
-
-})();
